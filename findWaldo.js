7f610cbb23e0650f3bd152b5c7db234d914fe1b1
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-09-27 15:13:53 +0000

    Refactor Find Waldo

--- findWaldo.js
+++ findWaldo.js
@@ -1,14 +1,9 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i, arr.length)
-      // console.log(i);   // execute callback
-    }
-  }
+  arr.forEach((name, index) => {if (name === "Waldo") actionWhenFound(index)})
 }
 
-function actionWhenFound(index, length) {
-  console.log("Found Waldo at index " + index + length );
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);

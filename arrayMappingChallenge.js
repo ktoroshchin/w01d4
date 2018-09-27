ee9f6f183fa181569d589c1bea7cae98a54b4acd
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-09-27 15:43:30 +0000

    Array mapping Challenge

---
+++ arrayMappingChallenge.js
@@ -0,0 +1,12 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(num => Math.sqrt(num.x * num.x + num.y * num.y));
+// console.log(result);
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
